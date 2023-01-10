5084c6297c7c56ec2442a2bad24e587d037ed835
JeongSoong <118896711+JeongSoong@users.noreply.github.com>
2023-01-10 16:31:12 +0900

    Update 박준성교수님과제2.c.c

--- 20230109/박준성교수님과제2.c.c
+++ 20230109/박준성교수님과제2.c.c
@@ -4,11 +4,10 @@
 
 int main(void)
 {
-	int i, j, temp = 0;
-	int tot = 0;
+	int i, j;
 	double avg;
     double average[4] = { 0 };
-	int scores[4][5];
+	int scores[3][5];
 	
 	for (i = 0; i < 4; i++)
         for (j = 0; j < 5; j++)
@@ -39,48 +38,54 @@ int main(void)
 	}
 	printf("\n\n");
 	
-	int score_rank[3][2];
-	int score_rank_who[3][2];
+	printf("===========================================");
+	printf("\n");
 	
-	for (int i = 0;i < 3;i++) 
+	int score_rank[i][j];
+	int score_rank_who[i][j];
+	
+	for (i=0; i<3; i++) 
 	{
-		for (int j = 0;j < 5;j++) {
-			if (score_rank[i][0] <= scores[i+1][j]) 
+		for (j=0; j<5; j++) 
+		{
+			if (score_rank[i][0] <= scores[i][j]) //1
 			{
 				score_rank[i][1] = score_rank[i][0];
-				score_rank[i][0] = scores[i+1][j];					
+				score_rank[i][0] = scores[i][j];					
 				score_rank_who[i][1] = score_rank_who[i][0];
 				score_rank_who[i][0] = j+1;
 			}
-			else if (score_rank[i][1] <= scores[i + 1][j]) 
+			else if (score_rank[i][1] <= scores[i][j]) 
 			{
-				score_rank[i][1] = scores[i + 1][j];
+				score_rank[i][1] = scores[i][j];
 				score_rank_who[i][1] = j+1;
 			}
 		}
 	}
 
 
-	for (int i = 0;i < 3;i++) 
+	for (i = 0;i < 3;i++) 
 	{
-		if (i == 0) printf("수학 등수\n");
-		if (i == 1) printf("국어 등수\n");
-		if (i == 2) printf("영어 등수\n");
+		if (i == 0) printf("국어 등수\n");
+		if (i == 1) printf("영어 등수\n");
+		if (i == 2) printf("수학 등수\n");
 		printf("1등 : %d번, %d점\n",score_rank_who[i][0],score_rank[i][0]);
 		printf("2등 : %d번, %d점\n", score_rank_who[i][1], score_rank[i][1]);
 		printf("\n\n");
 	}
 
+	printf("===========================================");
+	printf("\n");
 	
-	for (int i = 0;i < 3;i++) 
+	for (i=0; i<3; i++) 
 	{
-		int mean = 0;
-		if (i == 0) printf("수학 평균 = ");
-		if (i == 1) printf("국어 평균 = ");
-		if (i == 2) printf("영어 평균 = ");
-		for (int j = 0;j < 5;j++) mean += scores[i + 1][j];
-		mean /= 5;
-		printf("[%d]\n", mean);
+		int avg = 0;
+		if (i == 0) printf("국어 평균 = ");
+		if (i == 1) printf("영어 평균 = ");
+		if (i == 2) printf("수학 평균 = ");
+		for (j = 0; j < 5; j++) avg += scores[i][j];
+		avg /= 5;
+		printf("[%d]\n", avg);
 	}
 	
 }
