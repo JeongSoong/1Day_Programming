c5e4f6908e40777c43368e1dccc64232d189909a
JeongSoong <118896711+JeongSoong@users.noreply.github.com>
2024-01-09 23:12:13 +0900

    Add files via upload

---
+++ 20240108/Block.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.6.33815.320
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Block", "Block\Block.vcxproj", "{86415879-50C2-4DD9-87A9-A61018DDAC80}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Debug|x64.ActiveCfg = Debug|x64
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Debug|x64.Build.0 = Debug|x64
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Debug|x86.ActiveCfg = Debug|Win32
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Debug|x86.Build.0 = Debug|Win32
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Release|x64.ActiveCfg = Release|x64
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Release|x64.Build.0 = Release|x64
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Release|x86.ActiveCfg = Release|Win32
+		{86415879-50C2-4DD9-87A9-A61018DDAC80}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {56E69BF1-4798-4C68-8BE2-6CAE40C0F273}
+	EndGlobalSection
+EndGlobal
